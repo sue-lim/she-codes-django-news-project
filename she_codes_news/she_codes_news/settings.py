5bb26b3878b0bc119803c5ba5776ddd79591f87a
Sue Lim <113986306+sue-lim@users.noreply.github.com>
2022-12-04 18:04:02 +0800

    Commiting to git for laptop

--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'she_codes_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+import os
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
