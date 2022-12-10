c8f19ac971a83091062524c9233bd4770f884731
SushiNspice <113986306+sue-lim@users.noreply.github.com>
2022-12-10 16:05:07 +0800

    committing to GIT for desktop

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,9 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+# this is the model
 class NewsStory(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     pub_date = models.DateTimeField()
     content = models.TextField()
+    image_url = models.URLField(blank=True)
 #text field for longer text vs such as blogs
+
+# def get_absolute_url(self):
+#     return reverse('news:story', kwargs={'pk': self.pk})
